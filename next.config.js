8d003a66928fd5a8c4d5c26c16050626bb3273f2
Davon Burks <burksdavon29@yahoo.com>
2023-12-29 21:20:56 -0500

    Updating Config again

--- next.config.js
+++ next.config.js
@@ -1,17 +1,17 @@
-/** @type {import('next').NextConfig} */
+/**
+ * @type {import('next').NextConfig}
+ */
 const nextConfig = {
-  // Enables the new output file system that includes static HTML export.
-  // Replace 'standalone' with 'export' if you want to mimic the behavior of `next export`.
-  output: 'standalone',
-
-  // Add any other custom Next.js configurations here.
-  // For example, if you need to specify a base path or rewrites, you can do so.
-  // basePath: '/your-base-path',
-  // async rewrites() {
-  //   return [
-  //     // Your rewrite rules here
-  //   ];
-  // },
-};
-
-module.exports = nextConfig;
+  output: 'export',
+ 
+  // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
+  // trailingSlash: true,
+ 
+  // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
+  // skipTrailingSlashRedirect: true,
+ 
+  // Optional: Change the output directory `out` -> `dist`
+  // distDir: 'dist',
+}
+ 
+module.exports = nextConfig
